b2add795590fcd80ebce69226712ac9c80376aa8
Fredrik Öhrström <oehrstroem@gmail.com>
2024-11-26 21:18:20 +0100

    Improve bash completion script.

--- scripts/autocompletion_for_xmq.sh
+++ scripts/autocompletion_for_xmq.sh
@@ -1,11 +1,77 @@
-# Bash auto completion for xq
+# Bash auto completion for xmq
 
-_xq()
+_xmq()
 {
-    arg_index="${COMP_CWORD}"
-    if [[ "${arg_index}" -eq 1 ]]; then
-        COMPREPLY=($(compgen -W "--output=xmq --output=xml --output=html --output=json" -f -- "${COMP_WORDS[1]}"))
+    local cur prev opts
+    COMPREPLY=()
+    cur="$2"
+    prev="$3"
+    prevprev="${COMP_WORDS[COMP_CWORD-2]}"
+
+    compopt +o default
+
+    opts="--compact --html --htmq --ixml= --json --trim= --xmq --xml -z
+          to-clines to-html to-json to-xmq to-xml page browse"
+
+#    echo -e "\nCUR=>${cur}< PREV=>${prev}< PPREV=>${prevprev}<\n"
+
+    if [ "$prevprev" = "--trim" ] || [ "$prev" = "--trim" ]
+    then
+        COMPREPLY=( $(compgen -W "none default"  -- "${cur}") )
+	return 0
     fi
+
+    if [ "$prevprev" = "--ixml" ] || [ "$prev" = "--ixml" ]
+    then
+	COMPREPLY=( $(compgen -S .ixml -- "${cur}") )
+	compopt -o default
+	return 0
+    fi
+
+    COMPREPLY=( $(compgen -f -W "${opts}" -- "${cur}") )
+
+    if [[ "${COMPREPLY[@]}" =~ "=" ]]
+    then
+        # Do not add space after =
+        compopt -o nospace
+    fi
+
+    return 0
+
+    # Handle --xxxxxx=
+    if [[ ${prev} == "--"* && ${cur} == "=" ]]
+    then
+        compopt -o filenames
+        COMPREPLY=(*)
+        return 0
+    fi
+
+    # Handle --xxxxx=path
+    if [[ ${prev} == '=' ]]
+    then
+        # Unescape space
+        cur=${cur//\\ / }
+        # Expand tilder to $HOME
+        [[ ${cur} == "~/"* ]] && cur=${cur/\~/$HOME}
+        # Show completion if path exist (and escape spaces)
+        compopt -o filenames
+        local files=("${cur}"*)
+        [[ -e ${files[0]} ]] && COMPREPLY=( "${files[@]// /\ }" )
+        return 0
+    fi
+
+    COMPREPLY=( $(compgen -f -W "${opts}" -- "${cur}") )
+
+    return 0
+
+#    if [[ ${#COMPREPLY[@]} == 1 && ${COMPREPLY[0]} != "--"*"=" ]]
+#    then
+#        # If there's only one option, without =, then allow a space##
+#	compopt +o nospace +o filenames
+#        COMPREPLY=( $(compgen -f -- "${cur}" ) )
+#        return 0
+#    fi
+
 }
 
-complete -F _xq xq
+complete -F _xmq xmq
