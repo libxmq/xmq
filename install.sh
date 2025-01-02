9f90ec8369944e6076c6a816a19469a62f288bbb
Omoeba <38597972+Omoeba@users.noreply.github.com>
2025-01-02 01:10:10 -0800

    Improve installation compatability with Homebrew and macOS
    Signed-off-by: Omoeba <38597972+Omoeba@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-if [ "$1" = "" ] || [ "$1" = "-h" ]
-then
+if [ "$1" = "" ] || [ "$1" = "-h" ]; then
     echo "Usage: install.sh [build_dir] [dest_dir]
     Example: install.sh build/default/release /usr/local
     "
@@ -13,28 +12,42 @@ fi
 FROM="$1"
 DESTDIR="$2"
 
-if [ ! -f "$FROM/../spec.inc" ]
-then
+if [ ! -f "$FROM/../spec.inc" ]; then
     echo "Oups, please supply a build dir such as build/default/release"
     exit 1
 fi
 
-if [ ! -d "$DESTDIR" ]
-then
+if [ ! -d "$DESTDIR" ]; then
     echo "Oups, please supply a valid dest_dir directory."
     exit 1
 fi
 
 #. "${FROM}/../spec.inc"
 
-install -Dm 755 -s "${FROM}/xmq" "${DESTDIR}/bin/xmq"
-install -Dm 644 "${FROM}/libxmq.a" "${DESTDIR}/lib/libxmq.a"
-install -Dm 644 -s "${FROM}/libxmq.so" "${DESTDIR}/lib/libxmq.so"
+install_cmd="install -m 755 -s"
+install_lib_cmd="install -m 644"
+install_shared_lib_cmd="install -m 644 -s"
+
+if [ "$(uname)" = "Darwin" ]; then
+    install_cmd="install -m 755 -c"
+    install_lib_cmd="install -m 644 -c"
+    install_shared_lib_cmd="install -m 644 -c"
+fi
+
+# Ensure destination directories exist
+mkdir -p "${DESTDIR}/bin"
+mkdir -p "${DESTDIR}/lib"
+mkdir -p "${DESTDIR}/man/man1"
+mkdir -p "${DESTDIR}/share/bash-completion/completions"
+
+$install_cmd "${FROM}/xmq" "${DESTDIR}/bin/xmq"
+$install_lib_cmd "${FROM}/libxmq.a" "${DESTDIR}/lib/libxmq.a"
+$install_shared_lib_cmd "${FROM}/libxmq.so" "${DESTDIR}/lib/libxmq.so"
 mkdir -p "${DESTDIR}/include"
 cp "src/main/c/xmq.h" "${DESTDIR}/include/xmq.h"
 chmod 644 "${DESTDIR}/include/xmq.h"
-install -Dm 644 "doc/xmq.1" "${DESTDIR}/man/man1/xmq.1"
-install -Dm 644 "scripts/autocompletion_for_xmq.sh" "${DESTDIR}/share/bash-completion/completions/xmq"
+install -m 644 "doc/xmq.1" "${DESTDIR}/man/man1/xmq.1"
+install -m 644 "scripts/autocompletion_for_xmq.sh" "${DESTDIR}/share/bash-completion/completions/xmq"
 
 echo
 echo "xmq and libxmq sucessfully installed."
