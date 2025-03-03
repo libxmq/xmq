1b4aec1f6803a65fbd6fbe014bbaaca0bf5e8015
Fredrik Öhrström <oehrstroem@gmail.com>
2025-03-03 20:48:28 +0100

    2025-03-03: Version 3.3.2

--- dist/xmq.c
+++ dist/xmq.c
@@ -4354,7 +4354,7 @@ bool xmqParseFile(XMQDoc *doq, const char *file, const char *implicit_root, int 
 
 const char *xmqVersion()
 {
-    return "3.3.2-RC3";
+    return "3.3.2";
 }
 
 void do_whitespace(XMQParseState *state,
