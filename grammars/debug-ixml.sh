c1f9524ee2d6cfc1acc3f339288502aa8cdf61cf
Fredrik Öhrström <oehrstroem@gmail.com>
2025-07-20 12:30:29 +0200

    Work

--- grammars/debug-ixml.sh
+++ grammars/debug-ixml.sh
@@ -4,7 +4,7 @@ then
     echo "No such file: $1"
     exit 1
 fi
-./xmq --debug=ixml. $1 $2 $3 2> temp_log
+xmq --debug=ixml. $1 $2 $3 2> temp_log
 cat temp_log | grep '^(ixml' | grep -v "^(ixml....info)" > temp_render
-cat temp_render | XMQ_THEME=mono ./xmq grammars/xmq-utils/parse.ixml > temp_xmq
-cat temp_xmq | ./xmq transform --stringparam=level=debug grammars/xmq-utils/parse.xslq to-html br >/dev/null 2>&1
+cat temp_render | XMQ_THEME=mono xmq grammars/xmq-utils/parse.ixml > temp_xmq
+cat temp_xmq | xmq transform --stringparam=level=debug grammars/xmq-utils/parse.xslq to-html br >/dev/null 2>&1
--- grammars/trace-ixml.sh
+++ grammars/trace-ixml.sh
@@ -4,7 +4,7 @@ then
     echo "No such file: $1"
     exit 1
 fi
-./xmq --trace=ixml. $1 $2 $3 2> temp_log
+xmq --trace=ixml. $1 $2 $3 2> temp_log
 cat temp_log | grep '^(ixml' > temp_render
-cat temp_render | XMQ_THEME=mono ./xmq.working grammars/xmq-utils/parse.ixml > temp_xmq
-cat temp_xmq | ./xmq transform --stringparam=level=trace grammars/xmq-utils/parse.xslq to-html br >/dev/null 2>&1
+cat temp_render | XMQ_THEME=mono xmq grammars/xmq-utils/parse.ixml > temp_xmq
+cat temp_xmq | xmq transform --stringparam=level=trace grammars/xmq-utils/parse.xslq to-html br >/dev/null 2>&1
