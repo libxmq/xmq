eb2e1387e44099d0c3031cbeba8f60195b7d9af5
Fredrik Öhrström <oehrstroem@gmail.com>
2025-07-20 11:40:09 +0200

    Work

--- grammars/view-ixml.sh
+++ grammars/view-ixml.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-./xmq --debug=ixml. $1 $2 $3 2> >(grep '^(ixml' | grep -v " pred/" | grep -v "^(ixml.pa.info)" > temp_render)
-XMQ_THEME=mono ./xmq.working grammars/xmq-utils/parse.ixml temp_render transform grammars/xmq-utils/parse.xslq to-html br >/dev/null 2>&1
+if [ ! -f $1 ]
+then
+    echo "No such file: $1"
+    exit 1
+fi
+./xmq --debug=ixml. $1 $2 $3 2> temp_log
+cat temp_log | grep '^(ixml' | grep -v " none/" | grep -v "^(ixml....info)" > temp_render
+cat temp_render | XMQ_THEME=mono ./xmq.working grammars/xmq-utils/parse.ixml > temp_xmq
+cat temp_xmq | ./xmq transform --stringparam=level=view grammars/xmq-utils/parse.xslq to-html br >/dev/null 2>&1
