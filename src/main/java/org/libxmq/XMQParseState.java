407b5c7e10e29a0f89691923ba0089e5966cd747
Fredrik Öhrström <oehrstroem@gmail.com>
2025-10-31 15:37:49 +0100

    Parse elements.

--- src/main/java/org/libxmq/XMQParseState.java
+++ src/main/java/org/libxmq/XMQParseState.java
@@ -65,25 +65,28 @@ class XMQParseState
 
     void *default_namespace; // If xmlns=http... has been set, then a pointer to the namespace object is stored here.
 
-    // These are used for better error reporting.
-    const char *last_body_start;
-    size_t last_body_start_line;
-    size_t last_body_start_col;
-    const char *last_attr_start;
-    size_t last_attr_start_line;
-    size_t last_attr_start_col;
     */
+    // These are used for better error reporting.
+    int last_body_start_;
+    int last_body_start_line_;
+    int last_body_start_col_;
+
+    int last_attr_start_;
+    int last_attr_start_line_;
+    int last_attr_start_col_;
+
     int last_quote_start_;
     int last_quote_start_line_;
     int last_quote_start_col_;
-    /*
-    const char *last_compound_start;
-    size_t last_compound_start_line;
-    size_t last_compound_start_col;
-    const char *last_equals_start;
-    size_t last_equals_start_line;
-    size_t last_equals_start_col;
-    */
+
+    int last_compound_start_;
+    int last_compound_start_line_;
+    int last_compound_start_col_;
+
+    int last_equals_start_;
+    int last_equals_start_line_;
+    int last_equals_start_col_;
+
     int last_suspicios_quote_end_;
     int last_suspicios_quote_end_line_;
     int last_suspicios_quote_end_col_;
@@ -122,12 +125,21 @@ class XMQParseState
         i_++;
     }
 
-    private boolean is_xmq_token_whitespace(char c)
+    static private final boolean is_xmq_token_whitespace(char c)
     {
         if (c == ' ' || c == '\n' || c == '\t' || c == '\r')
         {
             return true;
         }
+        return false;
+    }
+
+    static private final boolean is_xmq_text_name(char c)
+    {
+        if (c >= 'a' && c <= 'z') return true;
+        if (c >= 'A' && c <= 'Z') return true;
+        if (c >= '0' && c <= '9') return true;
+        if (c == '-' || c == '_' || c == '.' || c == ':' || c == '#') return true;
         return false;
     }
 
@@ -139,6 +151,62 @@ class XMQParseState
     boolean is_xmq_entity_start(char c)
     {
         return c == '&';
+    }
+
+    boolean is_xmq_compound_start(char c)
+    {
+        return c == '(';
+    }
+
+    /** Check if a value can start with these two characters. */
+    boolean unsafe_value_start(char c, char cc)
+    {
+        return c == '&' || c == '=' || (c == '/' && (cc == '/' || cc == '*'));
+    }
+
+    int count_whitespace()
+    {
+        if (i_ > buffer_len_) return 0;
+
+        char c = buffer_.charAt(i_);
+
+        if (c == ' ' ||
+            c == '\n' ||
+            c == '\t' ||
+            c == '\r' ||
+            c == 0xa0 || // Unbreakable space
+            c == 0x2000 || // EN quad. &#8192; U+2000 utf8 E2 80 80
+            c == 0x2001 || // EM quad. &#8193; U+2001 utf8 E2 80 81
+            c == 0x2002 || // EN space. &#8194; U+2002 utf8 E2 80 82
+            c == 0x2003 // EM space. &#8195; U+2003 utf8 E2 80 83
+            )
+        {
+            return 1;
+        }
+
+        // If, in the future, there is any whitespace outside of the BMP
+        // (Basic Multilingual Plane) that does not fit in a single char.
+        // I.e. needs two surrogate paris, then we will return 2 here.
+
+        // But for now.
+        return 0;
+    }
+
+    boolean is_safe_value_char()
+    {
+        char c = buffer_.charAt(i_);
+        boolean is_ws = count_whitespace() > 0 ||
+        c == '\n' ||
+        c == '\t' ||
+        c == '\r' ||
+        c == '(' ||
+        c == ')' ||
+        c == '{' ||
+        c == '}' ||
+        c == '\'' ||
+        c == '"'
+        ;
+        return !is_ws;
     }
 
     private void eat_xmq_token_whitespace()
@@ -241,6 +309,72 @@ class XMQParseState
     void do_comment_continuation(int start_line, int start_col, int start, int stop, int stop_suffix)
     {
         System.out.print("[comment_continuation "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_element_key(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[element_key "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_element_name(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[element_name "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_element_ns(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[element_ns "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_colon(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[colon "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_apar_left(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[apar_left "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_apar_right(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[apar_right "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_brace_left(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[brace_left "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_brace_right(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[brace_right "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_equals(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[equals "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_attr_value_text(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[attr_value_text "+
+                         Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
+    }
+
+    void do_element_value_text(int start_line, int start_col, int start, int stop, int stop_suffix)
+    {
+        System.out.print("[element_value_text "+
                          Util.xmq_quote_as_c(buffer_, start, stop, true)+" "+start_line+":"+start_col+"]");
     }
 
@@ -368,7 +502,7 @@ class XMQParseState
         while (i_ < buffer_len_)
         {
             c = buffer_.charAt(i_);
-            if (!Text.is_xmq_text_name(c)) break;
+            if (!is_xmq_text_name(c)) break;
             if (!Text.is_lowercase_hex(c)) expect_semicolon = true;
             increment(c);
         }
@@ -543,6 +677,286 @@ class XMQParseState
         }
     }
 
+    boolean is_xmq_element_start(char c)
+    {
+        if (c >= 'a' && c <= 'z') return true;
+        if (c >= 'A' && c <= 'Z') return true;
+        if (c == '_') return true;
+        return false;
+    }
+
+    Quad<Integer,Integer,Integer,Integer> eat_xmq_text_name()
+    {
+        int text_start;
+        int text_stop;
+        int namespace_start;
+        int namespace_stop;
+        int colon = 0;
+
+        text_start = i_;
+
+        while (i_ < buffer_len_)
+        {
+            char c = buffer_.charAt(i_);
+            if (!is_xmq_text_name(c)) break;
+            if (c == ':') colon = i_;
+            increment(c);
+        }
+
+        if (colon > 0)
+        {
+            namespace_start = text_start;
+            namespace_stop = colon;
+            text_start = colon+1;
+        }
+        else
+        {
+            namespace_start = 0;
+            namespace_stop = 0;
+        }
+        text_stop = i_;
+
+        return new Quad(text_start, text_stop, namespace_start, namespace_stop);
+    }
+    void eat_xmq_text_value()
+    {
+        while (i_ < buffer_len_)
+        {
+            char c = buffer_.charAt(i_);
+            if (!is_safe_value_char()) break;
+            increment(c);
+        }
+    }
+
+    boolean peek_xmq_next_is_equal()
+    {
+        int i = i_;
+        char c = 0;
+
+        while (i < buffer_len_)
+        {
+            c = buffer_.charAt(i);
+            if (!is_xmq_token_whitespace(c)) break;
+            i++;
+        }
+        return c == '=';
+    }
+
+    void parse_xmq_text_value(XMQLevel level)
+    {
+        int start = i_;
+        int start_line = line_;
+        int start_col = col_;
+
+        eat_xmq_text_value();
+
+        int stop = i_;
+
+        assert(level != XMQLevel.LEVEL_XMQ);
+
+        if (level == XMQLevel.LEVEL_ATTR_VALUE)
+        {
+            do_attr_value_text(start_line, start_col, start, stop, stop);
+        }
+        else
+        {
+            do_element_value_text(start_line, start_col, start, stop, stop);
+        }
+    }
+
+    void parse_xmq_value(XMQLevel level)
+    {
+        char c = buffer_.charAt(i_);
+
+        if (is_xmq_token_whitespace(c)) { parse_xmq_whitespace(); c = buffer_.charAt(i_); }
+
+        if (is_xmq_quote_start(c))
+        {
+            parse_xmq_quote(level);
+        }
+        else if (is_xmq_entity_start(c))
+        {
+            parse_xmq_entity(level);
+        }
+        else if (is_xmq_compound_start(c))
+        {
+            // TODOparse_xmq_compound(state, level);
+        }
+        else
+        {
+            char cc = buffer_.charAt(i_);
+            if (unsafe_value_start(c, cc))
+            {
+                error_nr_ = XMQParseError.XMQ_ERROR_VALUE_CANNOT_START_WITH;
+                throw new XMQParseException(error_nr_);
+            }
+            parse_xmq_text_value(level);
+        }
+    }
+
+    void parse_xmq_element()
+    {
+        parse_xmq_element_internal(false, false);
+    }
+
+    void parse_xmq_element_internal(boolean doctype, boolean pi)
+    {
+        char c = 0;
+        // Name
+        int name_start = 0;
+        int name_stop = 0;
+        // Namespace
+        int ns_start = 0;
+        int ns_stop = 0;
+
+        int start_line = line_;
+        int start_col = col_;
+
+        /*
+        if (doctype)
+        {
+            Pair<Integer,Integer> p = eat_xmq_doctype();
+            name_start = p.left();
+            name_stop = p.right();
+        }
+        else if (pi)
+        {
+            Pair<Integer,Integer> p = eat_xmq_pi(state);
+            name_start = p.left();
+            name_stop = p.right();
+        }
+        else*/
+        {
+            Quad<Integer,Integer,Integer,Integer> q = eat_xmq_text_name();
+            name_start = q.first();
+            name_stop = q.second();
+            ns_start = q.third();
+            ns_stop = q.fourth();
+        }
+        int stop = i_;
+
+        // The only peek ahead in the whole grammar! And its only for syntax coloring. :-)
+        // key = 123   vs    name { '123' }
+        boolean is_key = peek_xmq_next_is_equal();
+
+        if (ns_start == 0)
+        {
+            // Normal key/name element.
+            if (is_key)
+            {
+                do_element_key(start_line, start_col, name_start, name_stop, stop);
+            }
+            else
+            {
+                do_element_name(start_line, start_col, name_start, name_stop, stop);
+            }
+        }
+        else
+        {
+            // We have a namespace prefixed to the element, eg: abc:working
+            int ns_len = ns_stop - ns_start;
+            do_element_ns(start_line, start_col, ns_start, ns_stop, ns_stop);
+            do_colon(start_line, start_col+ns_len, ns_stop, ns_stop+1, ns_stop+1);
+
+            if (is_key)
+            {
+                do_element_key(start_line, start_col+ns_len+1, name_start, name_stop, stop);
+            }
+            else
+            {
+                do_element_name(start_line, start_col+ns_len+1, name_start, name_stop, stop);
+            }
+        }
+
+
+        c = buffer_.charAt(i_);
+        if (is_xmq_token_whitespace(c)) { parse_xmq_whitespace(); c = buffer_.charAt(i_); }
+
+        if (c == '(')
+        {
+            int start = i_;
+            last_attr_start_ = i_;
+            last_attr_start_line_ = line_;
+            last_attr_start_col_ = col_;
+            start_line = line_;
+            start_col = col_;
+            increment('(');
+            stop = i_;
+            do_apar_left(start_line, start_col, start, stop, stop);
+
+            // TODO parse_xmq_attributes();
+
+            c = buffer_.charAt(i_);
+            if (is_xmq_token_whitespace(c)) { parse_xmq_whitespace(); c = buffer_.charAt(i_); }
+            if (c != ')')
+            {
+                error_nr_ = XMQParseError.XMQ_ERROR_ATTRIBUTES_NOT_CLOSED;
+                throw new XMQParseException(error_nr_);
+            }
+
+            start = i_;
+            int parentheses_right_start = i_;
+            int parentheses_right_stop = i_+1;
+
+            start_line = line_;
+            start_col = col_;
+            increment(')');
+            stop = i_;
+            do_apar_right(start_line, start_col, parentheses_right_start, parentheses_right_stop, stop);
+        }
+
+        c = buffer_.charAt(i_);
+        if (is_xmq_token_whitespace(c)) { parse_xmq_whitespace(); c = buffer_.charAt(i_); }
+
+        if (c == '=')
+        {
+            last_equals_start_ = i_;
+            last_equals_start_line_ = line_;
+            last_equals_start_col_ = col_;
+            int start = i_;
+            start_line = line_;
+            start_col = col_;
+            increment('=');
+            stop = i_;
+
+            do_equals(start_line, start_col, start, stop, stop);
+
+            parse_xmq_value(XMQLevel.LEVEL_ELEMENT_VALUE);
+            return;
+        }
+
+        if (c == '{')
+        {
+            int start = i_;
+            last_body_start_ = i_;
+            last_body_start_line_ = line_;
+            last_body_start_col_ = col_;
+            start_line = line_;
+            start_col = col_;
+            increment('{');
+            stop = i_;
+            do_brace_left(start_line, start_col, start, stop, stop);
+
+            parse_xmq();
+
+            c = buffer_.charAt(i_);
+
+            if (is_xmq_token_whitespace(c)) { parse_xmq_whitespace(); c = buffer_.charAt(i_); }
+            if (c != '}')
+            {
+                error_nr_ = XMQParseError.XMQ_ERROR_BODY_NOT_CLOSED;
+                throw new XMQParseException(error_nr_);
+            }
+
+            start = i_;
+            start_line = line_;
+            start_col = col_;
+            increment('}');
+            stop = i_;
+            do_brace_right(start_line, start_col, start, stop, stop);
+        }
+    }
+
     void parse_xmq()
     {
         while (i_ < buffer_len_)
@@ -554,8 +968,8 @@ class XMQParseState
             else if (is_xmq_quote_start(c)) parse_xmq_quote(XMQLevel.LEVEL_XMQ);
             else if (is_xmq_entity_start(c)) parse_xmq_entity(XMQLevel.LEVEL_XMQ);
             else if (is_xmq_comment_start(c, cc)) parse_xmq_comment(cc);
+            else if (is_xmq_element_start(c)) parse_xmq_element();
             /*
-            else if (is_xmq_element_start(c)) parse_xmq_element(state);
             else if (is_xmq_doctype_start(state->i, end)) parse_xmq_doctype(state);
             else if (is_xmq_pi_start(state->i, end)) parse_xmq_pi(state);
             else if (c == '}') return;
