e85c0eded073b14abb8bc9daac2ed99ef81bbdbb
Fredrik Öhrström <oehrstroem@gmail.com>
2025-11-19 12:54:20 +0100

    Add missing Line class.

---
+++ src/main/java/org/libxmq/imp/Line.java
@@ -0,0 +1,9 @@
+
+package org.libxmq.imp;
+
+/** A Line stores the index of the first (start) char of the line
+    and stores the index of the char efter the last char of the line (stop).
+    The (cr)nl is not included within start-stop. If the line ends with
+    a newline, then nl is true.
+*/
+record Line(int start, int stop, boolean trim, boolean nl) { }
