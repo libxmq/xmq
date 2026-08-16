3ed86e08744bfd6e737d86ca7de93615b416ac81
Fredrik Öhrström <oehrstroem@gmail.com>
2026-08-16 22:07:23 +0200

    Improve namespace testing.

--- tests/test_005_namespaces.c
+++ tests/test_005_namespaces.c
@@ -5,10 +5,11 @@
 
 char *test = __FILE__;
 
-void test_building_dom0()
+void test_building_doc_0()
 {
     // Build a document without namespace.
     const char *exp = "car{model=escargo color=green}\n";
+    // <car><model>escargo</model><color>green</color></car>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -42,10 +43,11 @@ void test_building_dom0()
     free(start);
 }
 
-void test_building_dom1()
+void test_building_doc_1()
 {
     // Build a document with a default namespace.
     const char *exp = "car(xmlns=urn:cargo){model=escargo color=green}\n";
+    // <car xmlns="urn:cargo"><model>escargo</model><color>green</color></car>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -79,10 +81,11 @@ void test_building_dom1()
     free(start);
 }
 
-void test_building_dom2()
+void test_building_doc_2()
 {
     // Build a document with a prefixed namespace.
     const char *exp = "abc:car(xmlns:abc=urn:cargo){abc:model=escargo abc:color=green}\n";
+    // <abc:car xmlns:abc="urn:cargo"><abc:model>escargo</model><abc:color>green</color></car>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -116,10 +119,11 @@ void test_building_dom2()
     free(start);
 }
 
-void test_building_dom3()
+void test_building_doc_3()
 {
-    // Use ancestor to find the proper uri.
+    // Use ancestor to find the proper namespace using only the uri.
     const char *exp = "coffee(xmlns=urn:shop){price=12.5}\n";
+    // <coffee xmlns="urn:shop"><price>12.5</price></coffee>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -151,10 +155,11 @@ void test_building_dom3()
     free(start);
 }
 
-void test_building_dom4()
+void test_building_doc_4()
 {
-    // Use ancestor to find the proper uri and use the found prefix.
+    // Use ancestor to find the namespace using only the uri. However the found namespace has a prefix, use it!
     const char *exp = "poof:coffee(xmlns:poof=urn:shop){bar(xmlns=urn:barrista){poof:price=12.5 woot=money}}\n";
+    // <poof:coffee xmlns:poof="urn:shop"><bar xmlns="urn:barrista"><poof:price>12.5</poof:price><woot>money</woot></poof:coffee>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -192,12 +197,13 @@ void test_building_dom4()
     free(start);
 }
 
-void test_building_dom5()
+void test_building_doc_5()
 {
     // We start with a default namespace urn:blue then later att an element
     // with an ancestor namespace with a prefix {c}urn:coffee.
     // This namespace does not exist and is added to the root node.
     const char *exp = "box(xmlns=urn:blue xmlns:c=urn:coffee){bar{c:price=99}}\n";
+    // <box xmlns="urn:blue" xmlns:c="urn:coffee"><bar><c:price>99</c:price></bar></box>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -234,12 +240,13 @@ void test_building_dom5()
     free(start);
 }
 
-void test_building_dom6()
+void test_building_doc_6()
 {
     // We start with a default namespace urn:blue then later add an element
     // with an ancestor namespace without a prefix urn:coffee.
     // This namespace does not exist and so a new unique prefix is selected.
     const char *exp = "box(xmlns=urn:blue xmlns:ns1=urn:coffee){bar{ns1:price=99}}\n";
+    // <box xmlns="urn:blue" xmlns:ns1="urn:coffee"><bar><ns1:price>99</ns1:price></bar></box>
 
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
@@ -276,13 +283,13 @@ void test_building_dom6()
     free(start);
 }
 
-void test_building_dom7()
+void test_building_doc_7()
 {
     // We start with a default namespace urn:blue then later
     // add key values with ANCESTOR namespaces. They are automatically
     // assigned unique prefixes.
     const char *exp = "box(xmlns=urn:blue xmlns:ns1=urn:soft xmlns:ns2=urn:bar){ns1:flower=many ns1:power=123 ns2:soft=petal}\n";
-
+    // <box xmlns="urn:blue" xmlns:ns1="urn:soft" xmlns:ns2="urn:bar"><ns1:flower>many</ns1:flower><ns1:power>123</ns1:power><ns2:soft>petal</ns2:soft></box>
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
     XMQDoc *doc = rd.doc;
@@ -317,14 +324,57 @@ void test_building_dom7()
     free(start);
 }
 
+void test_building_doc_8()
+{
+    // Add an explicit namespace before adding the child element. The NS_ANCESTOR
+    // will find this namespace.
+    const char *exp = "flower(xmlns=urn:power xmlns:s=urn:soft){s:petals=many}\n";
+    // <flower xmlns="urn:power" xmlns:s="urn:soft"><s:petals>many</s:petals></flower>
+
+    XMQReturnDoc rd = xmqNewDoc();
+    assert(rd.status == XMQ_OK);
+    XMQDoc *doc = rd.doc;
+
+    XMQReturnNode rn = xmqAddRootElement(doc, "flower", NS_HERE("urn:power"));
+    assert(rn.status == XMQ_OK);
+
+    XMQStatus rc = xmqAddNamespace(doc, rn.node, NS_HERE("{s}urn:soft"));
+    assert(rc == XMQ_OK);
+
+    XMQNode *robot = rn.node;
+    xmqAddKeyValue(doc, robot, "petals", "many", NS_ANCESTOR("urn:soft"));
+
+    XMQOutputSettings *os = xmqNewOutputSettings();
+
+    xmqSetCompact(os, true);
+    xmqSetEscapeNewlines(os, true);
+    xmqSetUseColor(os, false);
+    xmqSetOutputFormat(os, XMQ_CONTENT_XMQ);
+    xmqSetRenderFormat(os, XMQ_RENDER_PLAIN);
+
+    char *start, *stop;
+    xmqSetupPrintMemory(os, &start, &stop);
+    xmqPrint(doc, os);
+
+    xmqFreeOutputSettings(os);
+
+    if (strcmp(start, exp))
+    {
+        printf("Building of dom tree failed. Got: %s\nExpected: %s\n", start, exp);
+        exit(1);
+    }
+    free(start);
+}
+
 int main(int argc, char **argv)
 {
-    test_building_dom0();
-    test_building_dom1();
-    test_building_dom2();
-    test_building_dom3();
-    test_building_dom4();
-    test_building_dom5();
-    test_building_dom6();
-    test_building_dom7();
+    test_building_doc_0();
+    test_building_doc_1();
+    test_building_doc_2();
+    test_building_doc_3();
+    test_building_doc_4();
+    test_building_doc_5();
+    test_building_doc_6();
+    test_building_doc_7();
+    test_building_doc_8();
 }
