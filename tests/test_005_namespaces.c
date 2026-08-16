f3f18f43c3ff41edd0198fafa06ccc1cc251ca1c
Fredrik Öhrström <oehrstroem@gmail.com>
2026-08-16 13:03:03 +0200

    Improve namespace testing.

--- tests/test_005_namespaces.c
+++ tests/test_005_namespaces.c
@@ -5,8 +5,49 @@
 
 char *test = __FILE__;
 
+void test_building_dom0()
+{
+    // Build a document without namespace.
+    const char *exp = "car{model=escargo color=green}\n";
+
+    XMQReturnDoc rd = xmqNewDoc();
+    assert(rd.status == XMQ_OK);
+    XMQDoc *doc = rd.doc;
+    void *ns;
+
+    XMQReturnNode rn = xmqAddRootElement(doc, "car", NS_NONE);
+    assert(rn.status == XMQ_OK);
+    XMQNode *car = rn.node;
+    xmqAddKeyValue(doc, car, "model", "escargo", NS_PARENT);
+    xmqAddKeyValue(doc, car, "color", "green", NS_PARENT);
+
+    XMQOutputSettings *os = xmqNewOutputSettings();
+
+    xmqSetCompact(os, true);
+    xmqSetEscapeNewlines(os, true);
+    xmqSetUseColor(os, false);
+    xmqSetOutputFormat(os, XMQ_CONTENT_XMQ);
+    xmqSetRenderFormat(os, XMQ_RENDER_PLAIN);
+
+    char *start, *stop;
+    xmqSetupPrintMemory(os, &start, &stop);
+    xmqPrint(doc, os);
+
+    xmqFreeOutputSettings(os);
+
+    if (strcmp(start, exp))
+    {
+        printf("Error.\nExpected:\%s\nGot:\n%s\n", exp, start);
+        exit(1);
+    }
+    free(start);
+}
+
 void test_building_dom1()
 {
+    // Build a document with a default namespace.
+    const char *exp = "car(xmlns=urn:cargo){model=escargo color=green}\n";
+
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
     XMQDoc *doc = rd.doc;
@@ -32,10 +73,9 @@ void test_building_dom1()
 
     xmqFreeOutputSettings(os);
 
-    const char *exp = "car(xmlns=urn:cargo){model=escargo color=green}\n";
     if (strcmp(start, exp))
     {
-        printf("Building of dom tree failed. Got: %s\nExpected: %s\n", start, exp);
+        printf("Error.\nExpected:\%s\nGot:\n%s\n", exp, start);
         exit(1);
     }
     free(start);
@@ -43,15 +83,56 @@ void test_building_dom1()
 
 void test_building_dom2()
 {
+    // Build a document with a prefixed namespace.
+    const char *exp = "abc:car(xmlns:abc=urn:cargo){abc:model=escargo abc:color=green}\n";
+
     XMQReturnDoc rd = xmqNewDoc();
     assert(rd.status == XMQ_OK);
     XMQDoc *doc = rd.doc;
     void *ns;
 
-    XMQReturnNode rn = xmqAddRootElement(doc, "book", NS_NONE);
+    XMQReturnNode rn = xmqAddRootElement(doc, "car", NS_HERE_P("abc=urn:cargo"));
     assert(rn.status == XMQ_OK);
-    XMQNode *book = rn.node;
-    xmqAddKeyValue(doc, book, "name", "100 years of solitude", NS_PARENT);
+    XMQNode *car = rn.node;
+    xmqAddKeyValue(doc, car, "model", "escargo", NS_PARENT);
+    xmqAddKeyValue(doc, car, "color", "green", NS_PARENT);
+
+    XMQOutputSettings *os = xmqNewOutputSettings();
+
+    xmqSetCompact(os, true);
+    xmqSetEscapeNewlines(os, true);
+    xmqSetUseColor(os, false);
+    xmqSetOutputFormat(os, XMQ_CONTENT_XMQ);
+    xmqSetRenderFormat(os, XMQ_RENDER_PLAIN);
+
+    char *start, *stop;
+    xmqSetupPrintMemory(os, &start, &stop);
+    xmqPrint(doc, os);
+
+    xmqFreeOutputSettings(os);
+
+    if (strcmp(start, exp))
+    {
+        printf("Error.\nExpected:\%s\nGot:\n%s\n", exp, start);
+        exit(1);
+    }
+    free(start);
+}
+
+void test_building_dom3()
+{
+    // Use ancestor to find the proper uri.
+    const char *exp = "coffee(xmlns=urn:shop){price=12.5}\n";
+
+    XMQReturnDoc rd = xmqNewDoc();
+    assert(rd.status == XMQ_OK);
+    XMQDoc *doc = rd.doc;
+    void *ns;
+
+    XMQReturnNode rn = xmqAddRootElement(doc, "coffee", NS_HERE("urn:shop"));
+    assert(rn.status == XMQ_OK);
+    XMQNode *coffee = rn.node;
+    xmqAddKeyValue(doc, coffee, "price", "12.5", NS_ANCESTOR("urn:shop"));
 
     XMQOutputSettings *os = xmqNewOutputSettings();
 
@@ -66,10 +147,9 @@ void test_building_dom2()
 
     xmqFreeOutputSettings(os);
 
-    const char *exp = "book{name='100 years of solitude'}\n";
     if (strcmp(start, exp))
     {
-        printf("Building of dom tree failed. Got: %s\nExpected: %s\n", start, exp);
+        printf("Error.\nExpected:\%s\nGot:\n%s\n", exp, start);
         exit(1);
     }
     free(start);
@@ -77,6 +157,8 @@ void test_building_dom2()
 
 int main(int argc, char **argv)
 {
+    test_building_dom0();
     test_building_dom1();
     test_building_dom2();
+    test_building_dom3();
 }
